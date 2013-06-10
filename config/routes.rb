7453c6cb2d98ca0bf057aa9852476386b444f1ab
Thien Lam <thien.lam@eastagile.com>
2013-06-10 20:12:48 +0700

    Fix route error

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Vietmenu::Application.routes.draw do
+  root to: 'home#index'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
   devise_for :users
   ActiveAdmin.routes(self)
-
-  root to: 'home#index'
 end
